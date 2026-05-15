02d47fa64af9a63634c142738830bd87090c5206
Jake McAndrew <jake.mcandrew@wallington-dummer.com>
2026-05-15 14:27:20 +1000

    fixed config

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,9 @@ import type { NextConfig } from "next";
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
+  trailingSlash: true,
+  basePath: '/wd-new-webiste',
+  assetPrefix: '/wd-new-webiste/',
 }
 
 module.exports = nextConfig
